95f94f20fa56a124d3158a59ac3c87bf83f2ee55
Asraf Rosmadi <amirulasraf96915@gmail.com>
2026-05-08 17:00:51 +0800

    Add google services plugin for Firebase integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
